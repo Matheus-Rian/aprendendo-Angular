b500cd996d570f66bde50c889ff80726370881e6
Matheus Rian <souzamatheusrian@gmail.com>
2020-12-24 12:39:25 -0200

    Aprendendo angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
+
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { Error404Component } from './components/error-404/error-404.component';
+import { CourseModule } from './components/courses/course.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    Error404Component,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
